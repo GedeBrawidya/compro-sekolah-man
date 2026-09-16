d3f972bcc141d0506ad6aec43b25802e94444d29
gedebrawidya <gedepujaa9@gmail.com>
2026-09-16 16:43:11 +0700

    penyesuaian login dan register

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -7,7 +7,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
-import { register } from '@/routes';
+
 import { store } from '@/routes/login';
 import { request } from '@/routes/password';
 import PasskeyVerify from '@/components/passkey-verify';
@@ -161,16 +161,7 @@ export default function Login({ status, canResetPassword }: Props) {
                             {processing ? 'Memproses...' : 'Masuk ke Portal'}
                         </Button>
 
-                        <div className="text-center text-sm font-medium text-[#527365] pt-1">
-                            Belum punya akun?{' '}
-                            <TextLink
-                                href={register()}
-                                tabIndex={5}
-                                className="font-extrabold text-[#265243] hover:text-[#142921]"
-                            >
-                                Daftar sekarang
-                            </TextLink>
-                        </div>
+
                     </>
                 )}
             </Form>
